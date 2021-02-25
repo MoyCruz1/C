004af85179c34252139208169070bad7b7f46d8c
MoyCruz1 <72365105+MoyCruz1@users.noreply.github.com>
2021-02-25 14:22:12 -0600

    Update homework1.c

--- homework/homework1.c
+++ homework/homework1.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX 1000
+#define IN 1
+#define OUT 0
+
+int main(){
+
+	int i, l, n, m;
+	char input [MAX];
+	char output [MAX];
+
+	printf("insertar texto formato HTML:  ");
+	gets(input);
+	l= strlen(input);
+	n= OUT;
+	printf("output");
+
+	for (i=0; i<l ; ++i){
+
+		if (input [i] == '<')
+		n = IN;
+		if (input [i] == '>')
+		n = OUT;
+		if( n == OUT)
+		output[i] = input[i];
+
+		if (output[i] == '>' )
+		printf("%c" , output[i]);
+	}
+}
