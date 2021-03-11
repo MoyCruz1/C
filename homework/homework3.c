d04483da75d7fde3591edc96868a9b73a069ee31
MoyCruz1 <72365105+MoyCruz1@users.noreply.github.com>
2021-03-11 12:55:04 -0600

    Create homework3.c

---
+++ homework/homework3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int i;
+    char dig[20];
+
+    printf("Digite Numeros: ");gets(dig);
+
+    if(dig[0] == '[' || dig[0] == '(' || dig[0] == '"'){
+        for(i = 0; i < strlen(dig); i++){
+            printf("%c",dig[i]);
+        }
+       }
+    else{
+        for(i = 0; i < strlen(dig); i++){
+            dig[i] = 'X';
+     }
+        for(i = 0;i<strlen(dig); i++){
+            printf("%c",dig[i]);
+     }
+    }
+    printf("\n");
+    system("pause");
+    return 0;
+}
