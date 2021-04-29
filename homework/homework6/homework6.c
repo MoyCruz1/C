581bba7db4699b60087d73d9073d0908c65b74b6
MoyCruz1 <72365105+MoyCruz1@users.noreply.github.com>
2021-04-29 14:12:51 -0500

    Create homework6.c

---
+++ homework/homework6/homework6.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+
+char linea[MAX];
+
+char ins[MAX];
+
+int main()
+{
+	int c, i;
+	node* n = NULL;
+	node* arbol = NULL;
+	char user_account[MAX];
+	char access_code[MAX];
+	char temp1[MAX]; 
+	char temp2[MAX];
+
+	while((convert_to_line(user_account, access_code)) != EOF ) {
+		if (strstr(ins, "add")) {
+			i = 0;
+			while (i != '\t') {
+				temp1[i] = user_account[i];
+				temp2[i] = access_code[i];
+				i++;
+			}
+
+			n = insert(temp1, temp2, arbol, 0);
+			if (arbol == NULL) {
+				arbol = n;
+			}
+		} else if (strstr(ins, "del")) {
+			del(arbol, user_account, access_code);
+		} else {
+			show(arbol, '+');
+		}
+	}
+	return 0;
+}
