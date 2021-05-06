4e4498ff399a36bf327cc569ac6064e6514f930f
MoyCruz1 <72365105+MoyCruz1@users.noreply.github.com>
2021-05-06 12:40:58 -0500

    Create homework7.c

---
+++ homework/homework7/homework7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include "homework7.h"
+
+int adminStatus = 0;
+
+int main() {
+    FILE * database;
+    long int tp = time(0);
+    struct tm * normalTime = localtime(&tp);
+    welcome(normalTime);
+    if (isEmployee()) {
+        database = fopen("admin.txt", "r");
+        fclose(database);
+    } else {
+        fopen("employees.txt", "a");
+    }
+    return 0;
+}
