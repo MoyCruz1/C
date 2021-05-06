fd510f2cc6c4a66c12df8193172a5de5db344a06
MoyCruz1 <72365105+MoyCruz1@users.noreply.github.com>
2021-05-06 12:42:30 -0500

    Create aux.c

---
+++ homework/homework7/aux.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+void welcome(struct tm* ntime) {
+    printf("La fecha de hoy es: %d/%d/%d\n", ntime->tm_mday, ntime->tm_mon+1, ntime->tm_year+1900);
+    printf("La hora es: %d:%d\n", ntime->tm_hour, ntime->tm_min );
+}
+
+int isEmployee() {
+    int res = 1;
+    printf("Are you an Employee?, write 1 for employee, 0 for admin\n");
+    scanf("%d", &res);
+    return res;
+}
